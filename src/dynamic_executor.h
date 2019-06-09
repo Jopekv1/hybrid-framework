423b0cbdc1fafa986469249deb770bf190a522f1
Jopekv1 <33425484+Jopekv1@users.noreply.github.com>
2019-06-09 17:21:34 +0200

    Add TODOs
    Signed-off-by: Jopekv1 <33425484+Jopekv1@users.noreply.github.com>

--- src/dynamic_executor.h
+++ src/dynamic_executor.h
@@ -1,10 +1,17 @@
 #include <omp.h>
 
+//TODO:
+//wyliczac chunk_size na podstawie ilosci threadow/pamieci gpu
 #define HYBRID_FOR(init,condition,increment)														\
 		int i, id, chunk_size = 5;																	\
 		__pragma(omp parallel default(shared) private(i,id) schedule(dynamic, chunk_size))			\
-		for(init;condition;increment)		 														
+		for(init;condition;increment)		
 
+
+//TODO:
+//faktycznie zawolac podany kernel
+//znalezc workaround na zinkrementowanie "i" o chunk_size
+//sprawic aby kernel liczyl dla chunk_size konretnych elementow poczawszy od indexu i
 #define HYBRID_EXECUTE(kernel, ...)																	\
 		id = omp_get_thread_num();																	\
 		if(id == 0) {																				\
